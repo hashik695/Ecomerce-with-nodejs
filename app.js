aa6185fc31bab2f826d057e1c25cdf8ef55b5a13
hashik <mohamedhashik.kp@gmail.com>
2020-09-25 22:00:12 +0530

    mongo connect and data access

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var adminRouter = require("./routes/admin");
 
 var app = express();
 var fileUpload=require("express-fileupload")
+var db=require('./config/connection')
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
@@ -28,6 +29,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(fileUpload())
+db.connect((err)=>{
+  if(err) console.log("db error"+err)
+  else console.log("db connect")
+
+}
+)
 app.use("/", usersRouter);
 app.use("/admin", adminRouter);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var productHelper=require('../helpers/products-helpers')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   let products = [
@@ -37,11 +37,24 @@ router.get('/', function(req, res, next) {
   res.render('admin/view-products',{admin:true,products});
 });
   router.get('/add-products',(req,res)=>{
-    res.render('admin/add-products')
+    res.render('admin/add-products',{admin:true})
   });
   router.post('/add-products',(req,res)=>{
     console.log(req.body);
     console.log(req.files.Image);
+
+    productHelper.addProduct(req.body,(id)=>{
+      let image=req.files.Image
+      console.log(id);
+      image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+        if(err){
+          res.render("admin/add-products")
+        }else
+        console.log(err);
+       
+      })
+      
+    })
   })
 
 
