b2fd17cce05fc38b6636dca24773a1577739bbb8
hashik <mohamedhashik.kp@gmail.com>
2020-09-26 22:46:34 +0530

    Product view for admin and users

--- routes/admin.js
+++ routes/admin.js
@@ -3,38 +3,13 @@ var router = express.Router();
 var productHelper=require('../helpers/products-helpers')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let products = [
-    {
-      name: "IPHONE 11",
-      category: "mobile",
-      description: "this is new phone",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ6vH8yrDkeH-87FWpoObUsBeGowp4GDFT9EA&usqp=CAU",
-    },
-    {
-      name: "Redmi 11",
-      category: "mobile",
-      description: "this is new phone",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRhc533cFrjEl1Gx2PSiXzGDQiohQ17drHPMQ&usqp=CAU",
-    },
-    {
-      name: "Samsung",
-      category: "mobile",
-      description: "this is new phone",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS9nCmaumRLx4cAygMVP8G1XgkpneKfpnyPGQ&usqp=CAU",
-    },
-    {
-      name: "Windows",
-      category: "mobile",
-      description: "this is new phone",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtX2AvAcz1HTc2dE0tT_MryHTl5cwCyccgLQ&usqp=CAU",
-    },
-  ];
+  productHelper.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('admin/view-products',{admin:true,products});
+  })
 
-  res.render('admin/view-products',{admin:true,products});
+
+  
 });
   router.get('/add-products',(req,res)=>{
     res.render('admin/add-products',{admin:true})
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var productHelper=require('../helpers/products-helpers')
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
  
-  res.render("index", { products, admin: false });
+  productHelper.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('users/view-products',{user:true,products});
+  })
+
 });
 
 module.exports = router;
