f0bfc7209ad92b7e9110cf5e18e88a5a3211a3e6
hashik <mohamedhashik.kp@gmail.com>
2020-10-09 16:13:26 +0530

    cart view done

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,6 +1,8 @@
 var db=require('../config/connection')
 var collection=require('../config/collections')
 var bcrypt=require("bcrypt")
+const { resolve, reject } = require('promise')
+const objectID  = require('mongodb').ObjectID
 module.exports={
     doSignup:(userData)=>{
         return new Promise(async(resolve,reject)=>{
@@ -37,5 +39,64 @@ module.exports={
                 reject({status:false})
             }
         }) 
+    },
+    addToCart:(proId,userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItem=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectID(userId)})
+            if(cartItem){
+              db.get().collection(collection.CART_COLLECTION).updateOne({user:objectID(userId)},{
+                  $push:{products:objectID(proId)}
+              }
+              ).then((response)=>{
+                  resolve()
+              })
+            }else{
+                let cartObj={
+                    user:objectID(userId),
+                    products:[objectID(proId)]
+                }
+                db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response)=>{
+                    resolve()
+                })
+            }
+        })
+    },
+    getCartProduct:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+            let cartItems=await db.get().collection(collection.CART_COLLECTION).aggregate([
+                {
+                    $match:{user:objectID(userId)}
+                },
+                {
+                    $lookup:{
+                        from:collection.PRODUCT_COLLECTION,
+                        let:{prodList:'$products'},
+                        pipeline:[
+                            {
+                                $match:{
+                                    $expr:{
+                                        $in:['$_id',"$$prodList"]
+                                    }
+                                }
+                            }
+                        ],
+                        as:'cartItems'
+                    }
+                }
+            ]).toArray()
+            resolve(cartItems[0].cartItems)
+        })
+    },
+    getCartItem:(userId)=>{
+        return new Promise(async(resolve,reject)=>{
+             let count=0
+            let cart=await db.get().collection(collection.CART_COLLECTION).findOne({user:objectID(userId)})
+            if(cart){
+                count=cart.products.length
+
+            }
+            resolve(count)
+        })
     }
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,15 @@ const verifyLogin=(req,res,next)=>{
   }
 } 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/",async function(req, res, next) {
   let user=req.session.user
    console.log(user);
+   let cartCount=null
+   if(req.session.user){
+     cartCount=await userHelper.getCartItem(req.session.user._id)
+   }
   productHelper.getAllProducts().then((products)=>{
-    res.render('users/view-products',{products,user});
+    res.render('users/view-products',{products,user,cartCount});
   })
   
 
@@ -37,6 +41,9 @@ router.get('/signup',(req,res)=>{
 router.post("/signup",(req,res)=>{
  userHelper.doSignup(req.body).then((response)=>{
    console.log(response);
+   req.session.loggedIn=true
+   req.session.user=response
+   res.redirect('/')
  })
 
 })
@@ -56,8 +63,16 @@ router.get('/logout',(req,res)=>{
   req.session.destroy()
   res.redirect('/')
 })
-router.get('/cart',verifyLogin,(req,res)=>{
-  res.render('users/cart')
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelper.getCartProduct(req.session.user._id)
+  console.log(products);
+  res.render('users/cart',{products,user:req.session.user})
+})
+router.get('/add-to-cart/:id',(req,res)=>{
+  console.log("api call");
+  userHelper.addToCart(req.params.id,req.session.user._id).then(()=>{
+    res.json({status:true})
+  })
 })
 
 
